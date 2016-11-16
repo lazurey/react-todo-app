530dbda46a7958a72ee1aac347b86331bd64b1e8
Sause <lazureys@gmail.com>
2016-11-16 17:19:11 +0800

    hello world

---
+++ README.md
@@ -0,0 +1,11 @@
+# React Todo App
+
+
+## API
+It uses the API rmal from [Spring Boot Api](https://github.com/silverjava/rest-api-springboot).
+
+## Libraries
+
+- React
+- Redux
+- Webpack
---
+++ todo-app/client/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { Router, Route, browserHistory } from 'react-router'
+
+import TodoHome from './containers/TodoHome'
+import configure from './store'
+
+const store = configure()
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={TodoHome}>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
